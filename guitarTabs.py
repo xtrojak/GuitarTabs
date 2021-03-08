2cf2c7ce1911ec47f06d78f4379eb2bad37fc245
xtrojak <trojak@mail.muni.cz>
2021-03-08 13:48:15 +0100

    Implemented exporting in svg/pdf/png formats

--- guitarTabs.py
+++ guitarTabs.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, send_file
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 import click
 
+from app.libs.constants import FILE_TYPES, OUTPUT_MIMES
 from app.libs.form import SubmitForm
 from app.libs.image import draw_picture
 from app.libs.parsing import Parser
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 parser = Parser()
+
+
+@app.route("/export/<file_type>")
+def export(file_type):
+    FILE_TYPES[file_type](file_obj=open("static/pics/tabs.svg"),
+                          write_to="static/pics/tabs.{}".format(file_type.lower()))
+    return send_file("static/pics/tabs.{}".format(file_type.lower()), mimetype=OUTPUT_MIMES[file_type],
+                     as_attachment=True, attachment_filename="tabs.{}".format(file_type.lower()))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,20 +32,13 @@ def index():
     form = SubmitForm()
     user_image = url_for('static', filename='pics/blank.svg')
     if form.is_submitted():
-        if 'export_pdf' in request.form:
-            print("export_pdf")
-        elif 'export_svg' in request.form:
-            print("export_svg")
-        elif 'export_png' in request.form:
-            print("export_png")
-        elif 'submit_button' in request.form:
-            form.validate()
-            text = form.text.data
-            result = parser.parse(text).data
-            result = parser.transform(result)
-            if result.success:
-                draw_picture(result.data, form.title.data, form.bars.data)
-                user_image = url_for('static', filename='pics/tabs.svg')
+        form.validate()
+        text = form.text.data
+        result = parser.parse(text).data
+        result = parser.transform(result)
+        if result.success:
+            draw_picture(result.data, form.title.data, form.bars.data)
+            user_image = url_for('static', filename='pics/tabs.svg')
 
     return render_template('index.html', form=form, name=name, user_image=user_image)
 
