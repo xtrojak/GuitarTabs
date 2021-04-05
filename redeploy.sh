8c7c68cf0168601a55a4881453eef7caee82e354
xtrojak <trojak@mail.muni.cz>
2021-04-05 14:35:58 +0200

    Fixed redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -2,6 +2,6 @@
 
 git pull
 docker build -t simpletabs:latest .
-docker stop $(docker ps -q --filter ancestor=simpletabs)
+docker stop $(docker ps -q --filter "name=simpletabs")
 docker system prune -f
-docker run -d -p 0.0.0.0:5000:5000 simpletabs
+docker run -d -p 0.0.0.0:5000:5000 --name "simpletabs" simpletabs
